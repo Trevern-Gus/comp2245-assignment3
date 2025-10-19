e275f3e05be77161bb980f337a05d719bc4934d8
Trevern G. LAMONTAGNE <trevernlamontagne10@gmail.com>
2025-10-19 04:26:39 -0400

    added win and stop play after win

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,29 +1,51 @@
 window.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('#board div');
+    const status = document.querySelector('#status');
     let grid = Array(9).fill(null);
     let currentPlayer = 'X';
+    let gameOver = false;
+
+    const wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
     squares.forEach((square, index) => {
         square.classList.add('square');
 
-        square.addEventListener("mouseover" , () => {
+        square.addEventListener("mouseover", () => {
             square.classList.add('hover');
-        })
+        });
 
-        square.addEventListener("mouseout" , () => {
+        square.addEventListener("mouseout", () => {
             square.classList.remove('hover');
-        })
+        });
 
         square.addEventListener('click', () => {
-            if (square.textContent === '') {
+            if (!gameOver && square.textContent === '') {
                 square.textContent = currentPlayer;
                 square.classList.add(currentPlayer);
                 grid[index] = currentPlayer;
-                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            }
 
+                if (checkWinner(currentPlayer)) {
+                    status.textContent = `Congratulations! ${currentPlayer} is the Winner!`;
+                    status.classList.add('you-won');
+                    gameOver = true;
+                } else {
+                    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                }
+            }
         });
     });
-
+    function checkWinner(player) {
+        return wins.some(combination => {
+            return combination.every(index => grid[index] === player);
+        });
+    }
 });
-        
