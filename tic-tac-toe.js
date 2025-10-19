dd632a77c67e37945450e99047148bfd55e0bab5
Trevern G. LAMONTAGNE <trevernlamontagne10@gmail.com>
2025-10-19 03:40:34 -0400

    Player symbols

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('#board div');
-    squares.forEach(square => {
+    let grid = Array(9).fill(null);
+    let currentPlayer = 'X';
+
+    squares.forEach((square, index) => {
         square.classList.add('square');
+        square.addEventListener('click', () => {
+            if (square.textContent === '') {
+                square.textContent = currentPlayer;
+                square.classList.add(currentPlayer);
+                grid[index] = currentPlayer;
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        });
     });
 });
+        
